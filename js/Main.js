9071fa2a31c8e684484fccf87d48359deb08b5cf
Gamkedo <chris@gamkedo.com>
2021-01-16 14:30:36 -0800

    1 line comment fix, testing git to discord integration for flipperball

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-//Main for Xeno Jackers
+//Main for Flipperball
 window.onload = function() {
     window.addEventListener("focus", windowOnFocus);
     window.addEventListener("blur", windowOnBlur);
