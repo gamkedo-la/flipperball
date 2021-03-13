2a9e68d22ca3b281ee0cef64d705d2afa6905e20
TheHaad <fmuntaz@gmail.com>
2021-03-12 22:42:30 -0500

    speed up slot animation

--- js/GameObjects/SlotMachine.js
+++ js/GameObjects/SlotMachine.js
@@ -8,4 +8,15 @@ class SlotMachine extends GameObject {
         this.isAnimating = true;
     }
 
+    updateAnimation(deltaTime) {
+        if (!this.hasAnimation || !this.isAnimating) return;
+
+        this.currTiming += deltaTime*4.3;
+
+        if (this.currTiming >= this.frameTimes[this.currFrame]) {
+            this.currTiming = 0;
+            this.updateFrame();
+        }
+    }
+
 }
